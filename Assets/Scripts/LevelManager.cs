9bc657f6022f2961014d80ad32a93ac1f1e27f3f
xUnderGame <unaigiz1@gmail.com>
2024-03-13 17:50:11 +0100

    tiles

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,5 +1,7 @@
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using System.IO;
 
 public class LevelManager : MonoBehaviour
 {
@@ -17,10 +19,24 @@ public class LevelManager : MonoBehaviour
         // Getting tile references
         basicTile = Resources.Load<TileBase>("Tiles/Default");
         Debug.Log(basicTile.name);
+
+        // Set default tile at 0,0 (testing)
+        tilemapCollideable.SetTile(new Vector3Int(0, 0, 0), basicTile);
+        // SaveLevel("test");
     }
 
-    void Update()
+
+    public void SaveLevel(string level)
     {
-        tilemapCollideable.SetTile(new Vector3Int(0, 0, 0), basicTile);
+        // Default status
+        // var data = JsonUtility.FromJson(Resources.Load<TextAsset>("MainData").text);
+        var test = new { a = "a", b = 1 };
+        File.WriteAllText($"{Application.persistentDataPath}/{level}.level", JsonUtility.ToJson(test));
+    }
+
+    public void LoadLevel(string level)
+    {
+        levelGrid.GetComponentsInChildren<Tilemap>().ToList().ForEach(layer => layer.ClearAllTiles());
+        Debug.LogWarning(Resources.Load($"Levels/{level}").name);
     }
 }
