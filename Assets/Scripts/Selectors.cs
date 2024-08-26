bb8d662916eb6e1158cfdcf6f5e8e37290829f1a
xUnderGame <unaigiz1@gmail.com>
2024-08-27 00:05:25 +0200

    selectors now move based on distance

--- Assets/Scripts/Selectors.cs
+++ Assets/Scripts/Selectors.cs
@@ -1,12 +1,17 @@
+using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
 public class Selectors : MonoBehaviour
 {
-    internal RectTransform left = null;
-    internal RectTransform right = null;
+    internal RectTransform left;
+    internal RectTransform right;
+
     private GameObject tracking = null;
+    private RectTransform trackRT = null;
+    private Vector2 distanceRight = Vector2.zero;
+    private Vector2 distanceLeft = Vector2.zero;
+    private bool stopMoving = false;
 
     void Start()
     {
@@ -18,20 +23,38 @@ public class Selectors : MonoBehaviour
     void Update()
     {
         if (EventSystem.current.currentSelectedGameObject == null) return;
-        else if (tracking == EventSystem.current.currentSelectedGameObject) return;
+
+        // Slowly move towards the target position
+        if (tracking != null) MoveSelector();
+
+        // New object?
+        if (tracking == EventSystem.current.currentSelectedGameObject) return;
 
         // Get the new reference
         tracking = EventSystem.current.currentSelectedGameObject;
-        RectTransform rt = tracking.GetComponent<RectTransform>();
-        
-        // Debug.Log($"{rt.position.x}, {rt.rect.width}");
+        trackRT = tracking.GetComponent<RectTransform>();
+        stopMoving = false;
 
         // Move the selectors to the currently selected UI object
-        right.SetParent(rt);
-        left.SetParent(rt);
-        right.anchoredPosition = rt.rect.center + new Vector2(rt.rect.width + (right.rect.width / 2), 0);
-        left.anchoredPosition = rt.rect.center + new Vector2(-rt.rect.width - (left.rect.width / 2), 0);
+        distanceRight = trackRT.rect.center + new Vector2(trackRT.rect.width + (right.rect.width / 2), 0);
+        distanceLeft = trackRT.rect.center + new Vector2(-trackRT.rect.width - (left.rect.width / 2), 0);
+
+        right.SetParent(trackRT);
+        left.SetParent(trackRT);
         right.localScale = Vector3.one;
         left.localScale = Vector3.one * -1;
+    }
+
+    // Moves the selector to a target direction
+    private void MoveSelector()
+    {
+        if (stopMoving) return;
+
+        float rightSpeed = Vector2.Distance(right.anchoredPosition, distanceRight) * Time.deltaTime * 10f;
+        float leftSpeed = Vector2.Distance(left.anchoredPosition, distanceLeft) *  Time.deltaTime * 10f;
+        if (rightSpeed <= 0.001f && leftSpeed <= 0.001f) stopMoving = true;
+
+        right.anchoredPosition = Vector2.MoveTowards(right.anchoredPosition, distanceRight, rightSpeed);
+        left.anchoredPosition = Vector2.MoveTowards(left.anchoredPosition, distanceLeft, leftSpeed);
     }
 }
