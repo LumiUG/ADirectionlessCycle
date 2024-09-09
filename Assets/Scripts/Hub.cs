111a8f2d64aa00685f8aaf3d2084469422a8ccdd
xUnderGame <unaigiz1@gmail.com>
2024-09-09 03:26:28 +0200

    huge level hub improvements, locked levels, unfinished, and PROGRESS!

--- Assets/Scripts/Hub.cs
+++ Assets/Scripts/Hub.cs
@@ -1,28 +1,93 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Hub : MonoBehaviour
 {
-    public Checker checker;
+    public List<GameObject> worldHolders = new(capacity: 3);
     public GameObject worldHolder;
     public GameObject backButton;
+    public Checker checker;
+    public Text levelName;
 
-    private RectTransform holderRT;
     private readonly int[] positions = { 0, -1920, -3840, -5760 };
+    private readonly List<int> completedLevelsCount = new() { 2, 2, 2 };
+    private GameObject lastSelectedlevel = null;
+    private RectTransform holderRT = null;
     private int worldIndex = 0;
 
     private void Start()
     {
         EventSystem.current.SetSelectedGameObject(backButton);
         holderRT = worldHolder.GetComponent<RectTransform>();
+
+        // Set colors for locked levels
+        for (int i = 0; i < worldHolders.Count; i++)
+        {
+            // Check all levels present in the hub for completion
+            for (int j = 0; j < worldHolders[i].transform.childCount; j++)
+            {
+                Transform child = worldHolders[i].transform.GetChild(j);
+                var levelCheck = GameManager.save.game.levels.Find(level => level.levelID == $"{worldHolders[i].name}/{child.name}");
+                if (levelCheck == null) continue;
+
+                // Add an outline and add a completed count
+                if (levelCheck.completed)
+                {
+                    Outline o = child.AddComponent<Outline>();
+                    o.effectColor = Color.green;
+                    o.effectDistance = new(8, -8);
+                    completedLevelsCount[i]++;
+                }
+            }
+
+            // Sorry! We are looping again for available levels using the completed count!
+            for (int j = 0; j < completedLevelsCount[i]; j++)
+            { 
+                Transform child = worldHolders[i].transform.GetChild(j);
+                if (child) child.GetComponent<Image>().color = Color.white;
+            }
+        }
     }
+
+    // Cycle through levels
+    private void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject == null) return;
+
+        // Checking if you swapped levels
+        if (lastSelectedlevel == EventSystem.current.currentSelectedGameObject || !EventSystem.current.currentSelectedGameObject.transform.parent.name.StartsWith("W")) return;
+        lastSelectedlevel = EventSystem.current.currentSelectedGameObject;
+
+        // Set the preview text
+        string levelID = $"{lastSelectedlevel.transform.parent.name}/{lastSelectedlevel.name}";
+        var level = LevelManager.Instance.GetLevel(levelID, false, true);
+        if (level != null)
+        {
+            // Locked level?
+            if (AbsurdLockedLevelDetection(levelID)) SetLevelName("???");
+            else SetLevelName(level.levelName);
+        }
+        else SetLevelName("UNDER DEVELOPMENT");
+    }
+
+    // Set level name on the hub
+    public void SetLevelName(string newText) { levelName.text = $"[ {newText} ]"; }
 
     // Load level
     public void StaticLoadLevel(string levelName)
     {
         if (!LevelManager.Instance) return;
 
-        LevelManager.Instance.LoadLevel(levelName);
+        // Is the level locked?
+        if (AbsurdLockedLevelDetection(levelName)) { AudioManager.Instance.PlaySFX(AudioManager.tileDeath, 0.25f); return; }
+
+        // Does the level exist? (also loads it)
+        if (!LevelManager.Instance.LoadLevel(levelName)) { AudioManager.Instance.PlaySFX(AudioManager.tileDeath, 0.40f); return; }
+        
+        // Keep loading
         LevelManager.Instance.RefreshGameVars();
         LevelManager.Instance.RefreshGameUI();
         UI.Instance.ChangeScene("Game");
@@ -40,5 +105,14 @@ public class Hub : MonoBehaviour
 
         // Update checker direction
         checker.dirX = direction;
+    }
+
+    // Returns true if a level is locked.
+    public bool AbsurdLockedLevelDetection(string fullLevelID)
+    {
+        if (GameManager.Instance.IsDebug()) return false;
+
+        string[] levelSplit = fullLevelID.Split("/")[1].Split("-");
+        return completedLevelsCount[int.Parse(levelSplit[0]) - 1] < int.Parse(levelSplit[1]);
     }
 }
