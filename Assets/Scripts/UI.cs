4e7cdfdc7db8bd43ebde307f1344c348f583df02
xUnderGame <unaigiz1@gmail.com>
2025-02-22 02:29:29 +0100

    level changes (now on laptop!)

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -283,7 +283,7 @@ public class UI : MonoBehaviour
             // Custom handlings
             if (split[1] == "Industrial") { popup.SetPopup("I'm sure you can figure out this one yourself!"); return; }
             if (split[1] == "Fragment TwoH") { popup.SetPopup("I won't let you go deeper."); return; }
-            if (split[1] == "3-1") { popup.SetPopup("No."); return; }
+            if (split[1] == "3-1") { popup.SetPopup("You aren't getting in this easily."); return; }
             if (split[1] == "3-10") { popup.SetPopup("No hints here. You're on your own!"); return; }
         } else hintLevelID = null;
 
